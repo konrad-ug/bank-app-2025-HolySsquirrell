940f7928848597086fb9c8014c06794f79915927
Marcel Jaroszewicz <mjaroszewicz@INF.UG.EDU.PL>
2025-10-16 13:18:27 +0200

    balance dodany

--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -4,5 +4,7 @@ from src.account import Account
 class TestAccount:
     def test_account_creation(self):
         account = Account("John", "Doe")
+        assert account.balance == 0
         assert account.first_name == "John"
         assert account.last_name == "Doe"
+        
