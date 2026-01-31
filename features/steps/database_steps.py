c0e5be52bab45235c61c652a31587e6bddd8a729
HolySsquirrell <marcel86421577646@wp.pl>
2026-01-31 23:09:43 +0100

    Update database_steps.py

--- features/steps/database_steps.py
+++ features/steps/database_steps.py
@@ -6,9 +6,15 @@ URL = "http://localhost:5000"
 @when('I save all accounts to DB')
 def save_accounts(context):
     response = requests.post(URL + "/api/accounts/save")
+    print("STATUS:", response.status_code)
+    print("BODY:", response.text)
     assert response.status_code == 200
+
 
 @when('I load all accounts from DB')
 def load_accounts(context):
     response = requests.post(URL + "/api/accounts/load")
+    print("STATUS:", response.status_code)
+    print("BODY:", response.text)
     assert response.status_code == 200
+
